d289a5597150d70b5a8d4e75ddc3a1c3066e8e9b
JinWoo Kim <kjw040416@gmail.com>
2024-06-24 10:42:30 +0900

    2024.06.23

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,14 +1,21 @@
-import React, { forwardRef, useContext, useState } from 'react'
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Modal from './Modal';
+import React, { forwardRef, useContext, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import Modal from "./Modal";
+import { useParams } from "react-router-dom";
+import { subDays, format } from "date-fns";
 
-import { FcCalendar } from 'react-icons/fc';
-import { MdFormatListBulletedAdd, MdOutlinePlayCircleFilled, MdClose, MdHive } from 'react-icons/md';
-import { MusicPlayerContext } from '../context/MusicPlayerProvider';
+import { FcCalendar } from "react-icons/fc";
+import { MdFormatListBulletedAdd, MdOutlinePlayCircleFilled, MdClose, MdHive } from "react-icons/md";
+import { MusicPlayerContext } from "../context/MusicPlayerProvider";
+
+// Register the necessary Chart.js components
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const CustomInput = forwardRef(({ value, onClick }, ref) => (
     <button onClick={onClick} ref={ref}>
@@ -17,7 +24,19 @@ const CustomInput = forwardRef(({ value, onClick }, ref) => (
     </button>
 ));
 
+const formatDate = (date) => format(date, "yyyy-MM-dd");
+
+const getLastMonthDates = () => {
+    const dates = [];
+    for (let i = 1; i <= 30; i++) {
+        // 날짜 범위를 하루 전부터 한 달 전까지로 수정
+        dates.push(formatDate(subDays(new Date(), i)));
+    }
+    return dates;
+};
+
 const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDate, data }) => {
+    const { id } = useParams(); // id를 URL 파라미터에서 가져옴
     const { addTrackToList, addTrackToEnd, playTrack } = useContext(MusicPlayerContext);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,27 +44,63 @@ const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDa
 
     const [youtubeResults, setYoutubeResults] = useState([]);
     const [selectedTitle, setSelectedTitle] = useState(null);
+    const [chartData, setChartData] = useState(null);
+
+    const fetchRankingData = async (title) => {
+        const dates = getLastMonthDates();
+        const rankingData = [];
+
+        for (let date of dates) {
+            const url = `https://raw.githubusercontent.com/KIMJW04/music-chart/main/${id}/${id}100_${date}.json`;
+
+            try {
+                const response = await axios.get(url);
+                const items = response.data;
+
+                const item = items.find((item) => item.title === title);
+                if (item) {
+                    rankingData.push({ date, rank: parseInt(item.rank, 10) });
+                }
+            } catch (error) {
+                console.error(`Failed to fetch data for date: ${date}`, error);
+            }
+        }
+
+        setChartData({
+            labels: rankingData.map((data) => data.date),
+            datasets: [
+                {
+                    label: title,
+                    backgroundColor: "rgba(75,192,192,0.2)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderWidth: 2,
+                    data: rankingData.map((data) => data.rank),
+                },
+            ],
+        });
+    };
+
+    const handleItemClick = (item) => {
+        setSelectedTitle(item.title);
+        searchYoutube(item.title);
+        fetchRankingData(item.title);
+    };
 
     const searchYoutube = async (query) => {
         try {
-            const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+            const response = await axios.get("https://www.googleapis.com/youtube/v3/search", {
                 params: {
-                    part: 'snippet',
+                    part: "snippet",
                     q: query,
-                    type: 'video',
+                    type: "video",
                     maxResults: 5,
                     key: process.env.REACT_APP_YOUTUBE_API_KEY,
                 },
             });
             setYoutubeResults(response.data.items);
         } catch (error) {
-            console.error('YouTube 검색에 실패했습니다.', error);
+            console.error("YouTube 검색에 실패했습니다.", error);
         }
-    };
-
-    const handleItemClick = (title) => {
-        setSelectedTitle(title);
-        searchYoutube(title);
     };
 
     const handlePlayNow = (result) => {
@@ -54,7 +109,7 @@ const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDa
             videoID: result.id.videoId,
             imageURL: result.snippet.thumbnails.default.url,
             artist: result.snippet.channelTitle,
-            rank: 1
+            rank: 1,
         };
         addTrackToList(newTrack);
         playTrack(0);
@@ -66,10 +121,10 @@ const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDa
             videoID: result.id.videoId,
             imageURL: result.snippet.thumbnails.default.url,
             artist: result.snippet.channelTitle,
-            rank: 1
+            rank: 1,
         };
         addTrackToEnd(newTrack);
-        toast.success('리스트에 추가했습니다.');
+        toast.success("리스트에 추가했습니다.");
     };
 
     const handleAddToPlaylistClick = (result) => {
@@ -78,7 +133,7 @@ const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDa
             videoID: result.id.videoId,
             imageURL: result.snippet.thumbnails.default.url,
             artist: result.snippet.channelTitle,
-            rank: 1
+            rank: 1,
         });
         setIsModalOpen(true);
     };
@@ -93,65 +148,81 @@ const Chart = ({ title, showCalendar, selectedDate, onDateChange, minDate, maxDa
 
     return (
         <>
-            <section className='music-chart'>
+            <section className="music-chart">
                 <div className="title">
                     <h2>{title}</h2>
                     {showCalendar && (
-                        <div className='date'>
-                            <DatePicker
-                                selected={selectedDate}
-                                onChange={onDateChange}
-                                dateFormat="yyyy-MM-dd"
-                                minDate={minDate}
-                                maxDate={maxDate}
-                                customInput={<CustomInput />}
-                            />
+                        <div className="date">
+                            <DatePicker selected={selectedDate} onChange={onDateChange} dateFormat="yyyy-MM-dd" minDate={minDate} maxDate={maxDate} customInput={<CustomInput />} />
                         </div>
                     )}
                 </div>
                 <div className="list">
                     <ul>
                         {data.map((item, index) => (
-                            <li key={index} onClick={() => handleItemClick(item.title)}>
-                                <span className='rank'>#{item.rank}</span>
-                                <span className='img' style={{ backgroundImage: `url(${item.imageURL})` }}></span>
-                                <span className='title'>{item.title}</span>
+                            <li key={index} onClick={() => handleItemClick(item)}>
+                                <span className="rank">#{item.rank}</span>
+                                <span className="img" style={{ backgroundImage: `url(${item.imageURL})` }}></span>
+                                <span className="title">{item.title}</span>
                             </li>
                         ))}
                     </ul>
                 </div>
             </section>
             {youtubeResults.length > 0 && (
-                <section className='youtube-result'>
+                <section className="youtube-result">
                     <h3>🧑🏻‍💻 👉 "{selectedTitle}"에 대한 유튜브 검색 결과입니다.</h3>
                     <ul>
                         {youtubeResults.map((result, index) => (
                             <li key={index}>
-                                <span className='img' style={{ backgroundImage: `url(${result.snippet.thumbnails.default.url})` }}></span>
-                                <span className='title'>{result.snippet.title}</span>
-                                <span className='playNow' onClick={() => handlePlayNow(result)}>
-                                    <MdOutlinePlayCircleFilled /><span className='ir'>노래듣기</span>
+                                <span className="img" style={{ backgroundImage: `url(${result.snippet.thumbnails.default.url})` }}></span>
+                                <span className="title">{result.snippet.title}</span>
+                                <span className="playNow" onClick={() => handlePlayNow(result)}>
+                                    <MdOutlinePlayCircleFilled />
+                                    <span className="ir">노래듣기</span>
                                 </span>
-                                <span className='listAdd' onClick={() => handleAddToList(result)}>
-                                    <MdFormatListBulletedAdd /><span className='ir'>리스트 추가하기</span>
+                                <span className="listAdd" onClick={() => handleAddToList(result)}>
+                                    <MdFormatListBulletedAdd />
+                                    <span className="ir">리스트 추가하기</span>
                                 </span>
-                                <span className='chartAdd' onClick={() => handleAddToPlaylistClick(result)}>
-                                    <MdHive /><span className='ir'>나의 리스트에 추가하기</span>
+                                <span className="chartAdd" onClick={() => handleAddToPlaylistClick(result)}>
+                                    <MdHive />
+                                    <span className="ir">나의 리스트에 추가하기</span>
                                 </span>
                             </li>
                         ))}
                     </ul>
-                    <span className='close' onClick={() => setYoutubeResults([])}><MdClose /></span>
+                    {chartData && (
+                        <div className="chart">
+                            <h3>{selectedTitle}의 차트</h3>
+                            <div>
+                                <Line
+                                    data={chartData}
+                                    options={{
+                                        maintainAspectRatio: false,
+                                        scales: {
+                                            y: {
+                                                reverse: true,
+                                                beginAtZero: false,
+                                                ticks: {
+                                                    stepSize: 1,
+                                                },
+                                            },
+                                        },
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    )}
+                    <span className="close" onClick={() => setYoutubeResults([])}>
+                        <MdClose />
+                    </span>
                 </section>
             )}
             <ToastContainer />
-            <Modal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                onAddToPlaylist={handleAddToPlaylist}
-            />
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onAddToPlaylist={handleAddToPlaylist} />
         </>
-    )
-}
+    );
+};
 
-export default Chart
+export default Chart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import ChartSlider from '../components/ChartSlider';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
+import React, { useState, useEffect, useMemo } from "react";
+import ChartSlider from "../components/ChartSlider";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 
 const Home = () => {
     const getYesterdaysDate = () => {
@@ -15,29 +15,34 @@ const Home = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const formattedDate = selectedDate.toISOString().split('T')[0];
+    const formattedDate = selectedDate.toISOString().split("T")[0];
 
-    const chartUrls = useMemo(() => [
-        { id: 'melon', name: '멜론 차트', url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/melon/melon100_${formattedDate}.json` },
-        { id: 'bugs', name: '벅스 차트', url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/bugs/bugs100_${formattedDate}.json` },
-        { id: 'apple', name: '애플 차트', url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/apple/apple100_${formattedDate}.json` },
-        { id: 'billboard', name: '빌보드 차트', url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/billboard/billboard100_${formattedDate}.json` }
-    ], [formattedDate]);
+    const chartUrls = useMemo(
+        () => [
+            { id: "melon", name: "멜론 차트", url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/melon/melon100_${formattedDate}.json` },
+            { id: "bugs", name: "벅스 차트", url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/bugs/bugs100_${formattedDate}.json` },
+            // { id: 'apple', name: '애플 차트', url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/apple/apple100_${formattedDate}.json` },
+            { id: "billboard", name: "빌보드 차트", url: `https://raw.githubusercontent.com/KIMJW04/music-chart/main/billboard/billboard100_${formattedDate}.json` },
+        ],
+        [formattedDate]
+    );
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const results = await Promise.all(chartUrls.map(async (chart) => {
-                    const response = await fetch(chart.url);
-                    if (!response.ok) {
-                        throw new Error(`Failed to fetch data from ${chart.name}`);
-                    }
-                    return {
-                        id: chart.id,
-                        name: chart.name,
-                        data: await response.json()
-                    };
-                }));
+                const results = await Promise.all(
+                    chartUrls.map(async (chart) => {
+                        const response = await fetch(chart.url);
+                        if (!response.ok) {
+                            throw new Error(`Failed to fetch data from ${chart.name}`);
+                        }
+                        return {
+                            id: chart.id,
+                            name: chart.name,
+                            data: await response.json(),
+                        };
+                    })
+                );
                 setCharts(results);
                 setLoading(false);
             } catch (error) {
